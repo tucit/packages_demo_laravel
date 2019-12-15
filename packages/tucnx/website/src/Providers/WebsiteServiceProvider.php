bae92192407ad26108983fc25731cacd4db369ce
TucIT <xuantucit@gmail.com>
2019-12-15 12:25:46 +0700

    source code

---
+++ packages/tucnx/website/src/Providers/WebsiteServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Tucnx\Website\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebsiteServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        dd('I created package successfully !!');
+    }
+
+    public function register()
+    {
+
+    }
+}
